46bcdd4e718891aeb350d93b6be68848d93ab17c
Gleb Dovzhenko <dovjenko.g@gmail.com>
2026-05-03 20:10:55 +0700

    Trying an overlay

--- flake.nix
+++ flake.nix
@@ -1,5 +1,9 @@
 {
   inputs = { };
 
-  outputs = { self }: { };
+  outputs = _: {
+    overlays.default = final: prev: {
+      xrt_1 = final.python3Packages.callPackage ./pkgs/xrt/1.nix { };
+    };
+  };
 }
