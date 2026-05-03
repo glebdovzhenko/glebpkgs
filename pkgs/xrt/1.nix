e9a28eec1f67a1c87a53536b40c8ae746aab428f
Gleb Dovzhenko <dovjenko.g@gmail.com>
2026-05-03 20:29:25 +0700

    Bumping xrt to 1.6.2

--- pkgs/xrt/1.nix
+++ pkgs/xrt/1.nix
@@ -3,19 +3,49 @@
   buildPythonPackage,
   fetchFromGitHub,
   setuptools,
+  numpy_2,
+  scipy,
+  matplotlib,
+  pyqtwebengine,
+  pyqt5,
+  pyopencl,
+  pyopengl,
+  pyopengl-accelerate,
+  colorama,
+  sphinx,
+  sphinxcontrib-jquery,
+  distro,
+  sphinx-tabs,
 }:
 
-buildPythonPackage {
+buildPythonPackage rec {
   pname = "xrt";
-  version = "1.6.1";
+  version = "1.6.2";
   src = fetchFromGitHub {
+    inherit version;
     owner = "kklmn";
     repo = "xrt";
-    rev = "43a229b98c49669378e2e2127ba00ac9217de451";
-    sha256 = "sha256-BfMfAx/sN3Uf/JxY41DzDce0yAotFeI9o//wINeOVEM=";
+    tag = version;
+    sha256 = "sha256-8eWnvruXeoE0gRHgzHEhO/IgNk1pq9GM4ru3ditOyeA=";
   };
   pyproject = true;
   build-system = [ setuptools ];
+
+  propagatedBuildInputs = [
+    numpy_2
+    scipy
+    matplotlib
+    pyqtwebengine
+    pyqt5
+    pyopencl
+    pyopengl
+    pyopengl-accelerate
+    colorama
+    sphinx
+    sphinxcontrib-jquery
+    distro
+    sphinx-tabs
+  ];
 
   postInstall = ''
     makeWrapper $out/bin/xrtQookStart.py $out/bin/xrtQook --run "chmod -R 777 \$HOME/.xrt"
