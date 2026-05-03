e1d1b6e7653b52fa993a253ade506781bab55e28
Gleb Dovzhenko <dovjenko.g@gmail.com>
2026-05-03 14:53:49 +0700

    added xrt

---
+++ pkgs/xrt/1.nix
@@ -0,0 +1,23 @@
+{
+  python312,
+  buildPythonPackage,
+  fetchFromGitHub,
+}:
+
+buildPythonPackage {
+  pname = "xrt";
+  version = "1.6.1";
+  src = fetchFromGitHub {
+    owner = "kklmn";
+    repo = "xrt";
+    rev = "43a229b98c49669378e2e2127ba00ac9217de451";
+    sha256 = "sha256-BfMfAx/sN3Uf/JxY41DzDce0yAotFeI9o//wINeOVEM=";
+  };
+
+  postInstall = ''
+    makeWrapper $out/bin/xrtQookStart.py $out/bin/xrtQook --run "chmod -R 777 \$HOME/.xrt"
+    makeWrapper ${python312.interpreter} $out/bin/xrtBentXtal \
+    --add-flags "$out/lib/python3.12/site-packages/xrt/gui/xrtBentXtal.py" \
+    --run "chmod -R 777 \$HOME/.xrt"
+  '';
+}
